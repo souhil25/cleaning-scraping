b04166cf51c42fabc5922d498e4cad919a9c66ba
Imru <louddadsouhilabdelmalek@gmail.com>
2025-04-24 20:56:48 +0500

    init cmmt

--- main.py
+++ main.py
@@ -3,36 +3,52 @@ from bs4 import BeautifulSoup
 import re
 import string
 from collections import defaultdict
+import operator
 
 def cleanInput(input):
     input = re.sub('\n+', " ", input)
     input = re.sub('[[0-9]*]', " ", input)
+    input = re.sub(' +', " ", input)
     input = bytes(input, "UTF-8")
     input = input.decode("ascii", "ignore")
     cleanInput = []
-    input = input.split(' ')
+    input = input.upper().split(' ')
     for item in input:
         item = item.strip(string.punctuation)
         if len(item) > 1 or (item.lower() == 'a' or item.lower() == 'i'):
             cleanInput.append(item)
     return cleanInput
 
+
 def ngrams(input, n):
     input = cleanInput(input)    
-    output = []
+    output = {}
     for i in range(len(input)-n+1):
-        output.append(tuple((input[i:i+n])))
+        ngramTemp = " ".join(input[i:i+n])
+        if ngramTemp not in output:
+            output[ngramTemp] = 0 
+        output[ngramTemp] += 1
     return output
+def isCommon(ngram):
+    commonWords = ["the", "be", "and", "of", "a", "in", "to", "have", "it",
+        "i", "that", "for", "you", "he", "with", "on", "do", "say", "this",
+        "they", "is", "an", "at", "but","we", "his", "from", "that", "not",
+        "by", "she", "or", "as", "what", "go", "their","can", "who", "get",
+        "if", "would", "her", "all", "my", "make", "about", "know", "will",
+        "as", "up", "one", "time", "has", "been", "there", "year", "so",
+        "think", "when", "which", "them", "some", "me", "people", "take",
+        "out", "into", "just", "see", "him", "your", "come", "could", "now",
+        "than", "like", "other", "how", "then", "its", "our", "two", "more",
+        "these", "want", "way", "look", "first", "also", "new", "because",
+        "day", "more", "use", "no", "man", "find", "here", "thing", "give",
+        "many", "well"]
+    for word in ngram:
+        if word in commonWords:
+            return True
+    return False
 
-html = urlopen("http://en.wikipedia.org/wiki/Python_(programming_language)")
-bsObj = BeautifulSoup(html, features="lxml")
-content = bsObj.find("div", {"id":"mw-content-text"}).get_text()
-ngrams_list = ngrams(content, 2)
-ngrams_counts = defaultdict(int)
-for ng in ngrams_list :
-    ngrams_counts[ng] += 1
-
-sorted_ngrams = dict(sorted(ngrams_counts.items(), key=lambda item: item[1], reverse=True))
-print(sorted_ngrams)
-print("2-grams count is: ", len(sorted_ngrams))
-
+content = str(urlopen("http://pythonscraping.com/files/inaugurationSpeech.txt").read(),'utf-8')
+ngrams = ngrams(content, 2)
+sortedNGrams = sorted(ngrams.items(), key = operator.itemgetter(1), reverse=True)
+print(sortedNGrams)
+print(len(sortedNGrams))
